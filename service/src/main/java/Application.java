17f79d315a547745df2da9bd583c9747e447465d
Shrivastava <Sonal.Shrivastava@coxautoinc.com>
2020-08-14 01:07:51 -0700

    Sync

--- service/src/main/java/Application.java
+++ service/src/main/java/Application.java
@@ -4,6 +4,7 @@ import model.ScannedDocumentTransfer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
@@ -14,8 +15,8 @@ public class Application {
     private static final String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 
     //The JDBC connection URL which allows for Windows authentication is defined below.
-   private static final String jdbcURL = "jdbc:sqlserver://sacsqldev14.cm.fdielt.com:1433;databaseName=SquashQA;integratedSecurity=true";
-   //private static final String jdbcURL = "jdbc:sqlserver://sacsqldev13.cm.fdielt.com:1433;databaseName=CMSPerformance3;integratedSecurity=true";
+  // private static final String jdbcURL = "jdbc:sqlserver://sacsqldev14.cm.fdielt.com:1433;databaseName=SquashQA;integratedSecurity=true";
+   private static final String jdbcURL = "jdbc:sqlserver://sacsqldev13.cm.fdielt.com:1433;databaseName=CMSPerformance3;integratedSecurity=true";
 
     public static void main(String[] args) throws SQLException {
         System.out.println("Program started");
@@ -49,7 +50,7 @@ public class Application {
             //declare the statement object
             Statement sqlStatement = databaseConnection.createStatement();
 
-            execCommand(5, sqlStatement);
+            execCommand(1, sqlStatement);
 
             System.out.println("Closing database connection");
 
@@ -69,7 +70,7 @@ public class Application {
         if (count > 0 && count < 100 && sqlStatement != null) {
             System.out.println("************************************");
             System.out.println(" Batch Number  | Manufacturer Id");
-            Map<BatchTransfer, ScannedDocumentTransfer> values = DataGenerator.getData(count, "WA",
+            Map<BatchTransfer, ScannedDocumentTransfer> values = DataGenerator.getData(count, "MA",
                     "1", "HONDA");
             sqlStatement.execute("BEGIN TRANSACTION;");
             for (BatchTransfer batchTransfer: values.keySet()) {
@@ -102,7 +103,11 @@ public class Application {
             output.append(batchTransfer.getClientShortName()).append("', '");
             output.append(batchTransfer.getState()).append("', '");
             output.append(batchTransfer.getLegacyStatus()).append("', '");
-//            output.append("2010-03-23T11:30:19','03 TRIAD ','2010-03-23T11:30:19','1815079','Reader Auto-Commit'," +
+
+
+
+
+            //            output.append("2010-03-23T11:30:19','03 TRIAD ','2010-03-23T11:30:19','1815079','Reader Auto-Commit'," +
 //                    "'2010-03-23T00:00:00','T','1','1','1','1')");
             output.append(batchTransfer.getCreatedDateTime()).append("', '");
             output.append(batchTransfer.getTrackingId()).append("', '");
